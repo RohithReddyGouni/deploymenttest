83fd2a312cb6c46a89dee6c072570a5b9b4864a4
RohithReddy Gouni <grohithreddy9999@gmail.com>
2024-07-27 17:53:51 -0500

    Ignore Health of Controller

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -57,10 +57,13 @@ public class CourseController {
         }
     }
 
+    /*
     @GetMapping("/health")
     public String healthCheck() {
         return "UP";
     }
+    */
+
 
 
     @GetMapping("/welcome")
